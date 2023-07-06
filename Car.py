2536ecbb5cf4c9210e1218b1acddf91ce908de53
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-07 01:46:01 +0800

    done for creating class and for the constructor

---
+++ Car.py
@@ -0,0 +1,10 @@
+# create a class called car
+class Car:
+# create a constructor with data attributes of year model, make, and assign speed at constant 0
+    def __init__(self, year_model, make):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
+# create a method for acceleration
+# create a method for brake
+# return speed
--- Psuedocode.py
+++
@@ -1,5 +0,0 @@
-# create a class called car
-# create a constructor with data attributes of year model, make, and speed
-# create a method for acceleration
-# create a method for brake
-# return speed
